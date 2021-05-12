ebf2399b6f5e6f670626e4367d7a6c91b7245117
vishesh13byte <visheshrustagi8@gmail.com>
2021-05-12 13:24:07 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+    
+     let changeableUrl = url;
+
+     if(country){
+         changeableUrl = `${url}/countries/${country}`
+     }
+
+    try{
+           const {data : {confirmed, recovered, deaths, lastUpdate}} = await axios.get(changeableUrl); 
+           return {confirmed, recovered, deaths, lastUpdate}
+           
+    }catch(e){
+       console.error(e);
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+         return data.map(({ confirmed, deaths , reportDate: date }) => ({
+            confirmed: confirmed.total,
+            deaths: deaths.total,
+            date: date
+        }));
+        
+        
+    }catch(e){
+       return e;
+    }
+
+}
+
+
+export const fetchCountries = async () => {
+
+    try{
+        const { data: {countries} }= await axios.get(`${url}/countries`);   
+
+        return countries.map((country) => country.name);
+
+    }catch (e){
+       return e;
+    }
+}
